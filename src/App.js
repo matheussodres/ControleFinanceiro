d4294ae06fce029472f6bae9106b0ac4d8a4eafa
Matheus Sodre <matheusssodre98@gmail.com>
2023-02-02 19:20:56 -0300

    update index

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Resume from "./components/Resume";
+import Global from "./styles/global";
+
+const App = () => {
+    return (
+    <>
+    <Header />
+    <Resume />
+    <Form />
+    <Global />
+    </>
+    );
+}
+
+export default App
